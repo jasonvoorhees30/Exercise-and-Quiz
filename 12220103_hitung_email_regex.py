128c7d61eec3924854dd5fe0c32ae46ef037237c
muhnaufalaz <94182186+muhnaufalaz@users.noreply.github.com>
2021-11-12 21:35:19 +0700

    Update 12220103_hitung_email_regex.py

--- 12220103_hitung_email_regex.py
+++ 12220103_hitung_email_regex.py
@@ -1,22 +1,17 @@
-# TUGAS 9 PEMROGRAMAN KOMPUTER K-01 
-# MUHAMMAD NAUFAL AQIL ZUHDI (12220103)
-# 16 NOVEMBER 2021
-# PROGRAM SORT JUMLAH ALAMAT EMAIL DENGAN REGEX
-
-#Memanggil library regular expression
+#Calling the regular expression library
 import re
 
-#Membuka file mbox-short.txt
+#Open the mbox-short.txt file
 fhand = open("mbox-short.txt")
 
-#Membuat list untuk menyimpan alamat email dari baris yang diawali dengan 'From ' dengan bantuan regex
+#Create a list to store email addresses from lines starting with 'From ' with the help of regex
 list = []
 for line in fhand:
     x = re.findall('^From ([a-zA-Z0-9]\S*@\S*[a-zA-Z0-9])', line)
     if len(x) > 0:
         list.append(x[0])     
         
-#Membuat dictionary berisi key dan value untuk masing-masing alamat email
+#Create a dictionary containing keys and values for each email address
 counts = {}
 for word in list:
     if word not in counts:
@@ -24,15 +19,15 @@ for word in list:
     else:
         counts[word] = counts.get(word,0) + 1
         
-#Membuat tuple dengan mempertimbangkan parameter (value, key) pada dictionary counts lalu memasukkannya ke dalam tmp
+#Create a tuple by considering the parameters (value, key) in the counts dictionary and then inserting it into tmp
 tmp = []
 for key, value in counts.items():
     newtup = (value, key)
     tmp.append(newtup)
 
-#Melakukan sort terhadap tmp untuk mendapatkan urutan nilai dari yang tertinggi
+#Sort tmp to get the order of values ​​from highest
 tmp = sorted(tmp, reverse = True)
 
-print("Urutan alamat email unik pada mbox-short.txt dengan jumlah terbanyak adalah sebagai berikut:")
+print("The order of unique email addresses in mbox-short.txt with the highest number is as follows:")
 for v, k in tmp:
-    print("Alamat email", k, "dengan jumlah sebanyak", v)
+    print("Email address", k, "with an amount of", v)
